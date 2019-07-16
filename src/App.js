eb3384a9915b864aab88ec7a30d8f0b7ba6ad964
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-07-16 16:36:50 +1000

    Code Splitting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
-import Details from "./Details";
 import SearchParams from "./SearchParams";
 import ThemeContext from "./ThemeContext";
+
+//Dynamic import with code splitting
+//In this case, it is not worth it. But for futur projects, you should do code spliiting
+//for code that accounts for at least 30KB+.
+const Details = lazy(() => import("./Details"));
 
 const App = () => {
   const theme = useState("darkblue");
@@ -13,11 +17,12 @@ const App = () => {
         <header>
           <Link to="/">Adopt Me!</Link>
         </header>
-        ;
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
+        <Suspense fallback={<h1>loading route...</h1>}>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
